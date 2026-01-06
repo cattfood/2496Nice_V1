cbcd13befb319d02c4d9a701d734e5013e4f0922
Alice <136747729+cattfood@users.noreply.github.com>
2026-01-05 16:30:55 -0800

    driver curve

--- src/main.cpp
+++ src/main.cpp
@@ -115,9 +115,9 @@ void opcontrol() {
 	
 		
 		int y = controller.get_analog(ANALOG_LEFT_Y);
-		int power = (y*abs(y)*sqrt(abs(y)))/(sqrt(127*16129));
+		int power = (y*y*y) / 16129;
 	    int x = controller.get_analog(ANALOG_RIGHT_X);
-		int turn = (x*abs(x)*sqrt(abs(x)))/(sqrt(127*16129));
+		int turn = (x*x*x) / 16129;
 		int left = power + turn;
 		int right = power - turn;
 		/*
