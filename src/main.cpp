237ab387938f22bad808d6371e0d317a0bdfbc4f
docpenguin44 <aquamattc3@gmail.com>
2026-01-10 11:58:26 -0800

    awp

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,7 @@ void autonomous() {
 	/*
 	//elims
 	desc.set_value(true);
-	forward_move(600, 600, 1,1);
+	forward_move(600, 800, 0.5,600);
 	tint.move(127);
 	bint.move(-127);
 	turnp(-51, 600);
@@ -33,26 +33,22 @@ void autonomous() {
 	pros::delay(250);
 	forward_move(-675, 600, 1,1);
 	turnp(-147, 600);
-	forward_move(1500, 1500, 1,1);
+	forward_move(1500, 1800, 1,1);
 	turnp(180, 400, {1.05, 0, 0}, {19.9, 0, 0});
 	forward_move(-1300, 1600, 0.3,800);
 	tint.move(-127);
-	bint.move(-127);
 	pros::delay(1750);
-	matchp.set_value(true);
+	matchp.set_value(true); 
 	tint.move(127);
-	bint.move(-127);
 	pros::delay(400);
 	turnp(181, 200);
-	forward_move(1150, 900, 0.6, 1000);
-	chassis_move(200, 200);
-	pros::delay(1200);
+	forward_move(1100, 900, 0.6, 1000);
+	chassis_move(40, 40);
+	pros::delay(1000);
 	chassis_move(0,0);
 	forward_move(-400, 500, 1,1);
 	tint.move(-127);
-	bint.move(-127);
 	forward_move(-1400, 1700, 0.3,600);
-	pros::delay(1000);
 	matchp.set_value(false);
 	chassis_move(-200,-200);
 */
@@ -77,83 +73,80 @@ void autonomous() {
 	//forward_move(800, 2000, 1, 1);
 
 	//------------------------------ awp
-	/*
+
 	turnp(0, 10);
-	forward_move(1350, 1300, 1, 1);
-	turnp(-90, 1000);
+	forward_move(1375, 1800, 1, 1);
+	turnp(-90, 500);
 	matchp.set_value(true);
+	mpist.set_value(true);
 	pros::delay(200);
 	bint.move(-127);
 	tint.move(127);
-	mpist.set_value(true);
-	und.set_value(false);
-	forward_move(700, 1000, 1, 1);
-	turnp(-93, 10);
-	forward_move(-400, 400, 1, 600);
-	//turnp(-90, 300);
+	chassis_move(30, 30);
+	pros::delay(1200);
+	turnp(-91, 500);
 	forward_move(-900, 1000, 1, 600);
-	matchp.set_value(false);
-	//forward_move(-900, 3000, 1,1);
+	chassis_move(-70, -70);
 	tint.move(-127);
-	mpist.set_value(true);
-	chassis_move(-30, -30); // -50, -50
-	//forward_move(-1000, 1000, 0.6,1000);
-	//turnp(-90, 400);
-	pros::delay(1500);
-	tint.move(127);
-	mpist.set_value(true);
+	matchp.set_value(false);
+	pros::delay(2000);
 	turnp(-90, 100);
-	forward_move(1000, 1000, 1,1);
-	turnp(-225, 1500, {1, 0, 7.6}, {19.9, 0, 0});
-	forward_move(1400, 1400, 1, 800);
-	turnp(-40, 1500, {1, 0, 8.67}, {19.9, 0, 0});
+	tint.move(127);
+	forward_move(1050, 1800, 1,1);
+	//turnp(-225, 100);
+	turnp(-225, 1200, {1, 0, 7.95}, {19.9, 0, 0});
+
+	forward_move(900, 1200, 0.5, 400);
+	chassis_move(70, 70);
+	pros::delay(500);
+	//turnp(-40, 100);
+	turnp(-40, 1300, {1, 0, 7.95}, {19.9, 0, 0});
 	bint.move(127);
 	tint.move(0);
 	pros::delay(75);
 	bint.move(0);
-	forward_move(-1200, 1200, 1, 400);
+	forward_move(-400, 1400, 1, 400);
 	mpist.set_value(false);
 	bint.move_velocity(-450);
 	tint.move_velocity(550);
 	pros::delay(800);
-	forward_move(920, 1000, 1, 1);
+	forward_move(870, 1000, 1, 1);
 	mpist.set_value(true);
-	turnp(-171, 900, {1, 0, 7.4}, {19.9, 0, 0});
-pros::delay(120);
+	turnp(-175, 900, {1, 0, 7.4}, {19.9, 0, 0});
+	pros::delay(120);
 	bint.move(-127);
-	tint.move(127);
-	//forward_move(1500, 1000, 1,800, true, {0.15, 0, 0},  {2.5, 0, 0});
-	//700 0.5 
+	tint.move(127); 
 	forward_move(2000, 1400, 0.5,800);
-
 	turnp(220, 800, {1.1, 0, 0}, {19.9, 0, 0});
-	forward_move(1500, 1800, 1,700);
-	turnp(-90, 1000,{1, 0, 0}, {19.9, 0, 0});
+	forward_move(1300, 1800, 1,700);
+	turnp(-90, 1000);
 	forward_move(-700, 800, 1, 600);
 	matchp.set_value(false);
 	tint.move(-127);
 	mpist.set_value(true);
 	chassis_move(-50, -50);
 	pros::delay(3000);
+	
 
 
 			//und.set_value(true);
 			//mpist.set_value(!true);
 	
-	pros::delay(4000);
-	*/
+
+
 	//skills
+	/*
 	desc.set_value(true);
 	turnp(0, 100);
-	forward_move(1800, 1100, 1, 1);
-	turnp(90, 600);//, {1, 0, 6.25} ,{19.9, 0, 0});
+	forward_move(1800, 1500, 1, 1);
+	turnp(89, 600);//, {1, 0, 6.25} ,{19.9, 0, 0});
 	matchp.set_value(true);
 	pros::delay(500);
 	bint.move(-127);
 	tint.move(127);
 	mpist.set_value(true);
 	forward_move(275, 325, 1, 1);
-	chassis_move(50, 50);
+	chassis_move(30, 30);
 	pros::delay(2500);
 	forward_move(-600, 700, 1,1);
 	matchp.set_value(false);
@@ -173,7 +166,7 @@ pros::delay(120);
 	pros::delay(2000);
 	chassis_move(0, 0);
 	turnp(-91, 500);
-	forward_move(300, 500, 1, 1);
+	forward_move(500, 600, 1, 1);
 	matchp.set_value(true);
 	pros::delay(300);
 	bint.move(-127);
@@ -197,7 +190,7 @@ pros::delay(120);
 	pros::delay(500);
 	bint.move(-127);
 	tint.move(127);
-	forward_move(275, 325, 1, 1);
+	forward_move(375, 475, 1, 1);
 	chassis_move(50, 50);
 	pros::delay(2500);
 	forward_move(-600, 800, 1,1);
@@ -208,7 +201,7 @@ pros::delay(120);
 	forward_move(1375, 1100, 1,1);
 	forward_move(1375, 1100,1,1 );
 	turnp(135, 500);
-	forward_move(850, 1100, 1,1);
+	forward_move(725, 1000, 1,1);
 	turnp(86, 700);
 	forward_move(-850, 1300, 0.5,1);
 	bint.move(-127);
@@ -236,6 +229,8 @@ pros::delay(120);
 	forward_move(-1300, 1500, 1, 1);
 	turnp(-178, 1200);
 	forward_move(-3000, 6000, 1, 1);
+	*/
+	
 }
 /**
  * Runs initialization code. This occurs as soon as the program is started.
@@ -390,7 +385,7 @@ void opcontrol() {
 
 
 		if (controller.get_digital_new_press(E_CONTROLLER_DIGITAL_A)) {
-			autonomous();
+			//autonomous();
 		}
 
 	lf.move(left);
